0c4e614f75dbf8268041fdcf2539ffcbf6e6020e
leverest96 <leverest96@gmail.com>
2023-05-18 10:57:21 +0900

    first commit

---
+++ src/test/java/site/recofit/ssafit/SsafitApplicationTests.java
@@ -0,0 +1,13 @@
+package site.recofit.ssafit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
