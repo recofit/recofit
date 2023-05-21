c6170e52656b6ac4ffb7717c454ad9e489aca9f8
borussen <81274632+borussen@users.noreply.github.com>
2023-05-22 07:48:34 +0900

    [CHORE/front] : 메인 페이지 문구 수정

--- Frontend/src/components/main/CarouselSection.vue
+++ Frontend/src/components/main/CarouselSection.vue
@@ -12,14 +12,14 @@
                 <img :src="require(`@/assets/img/carousel2.jpg`)" class="d-block w-100" alt="..." />
                 <div class="carousel-caption banner-row" style="color:white">
                   <h1>경험을 공유해주세요</h1>
-                  <p>당신의 경험이 누군가에게 도움이 될 것 입니다</p>
+                  <p>당신의 경험이 누군가에게 도움이 됩니다</p>
                 </div>
             </div>
             <div class="carousel-item">
                 <img :src="require(`@/assets/img/carousel3.jpg`)" class="d-block w-100" alt="..." />
                 <div class="carousel-caption banner-row" style="color:white">
                   <h1>계획을 세워보세요</h1>
-                  <p>더 오래, 더 빠르게 달릴 수 있을 것입니다</p>
+                  <p>더 오래, 더 빠르게 달릴 수 있습니다</p>
                 </div>
             </div>
         </div>
